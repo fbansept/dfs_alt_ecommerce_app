e4585c7ce958a884fe430cccc3491cb21c57ce60
fbansept <bansept.franck@gmail.com>
2022-08-31 15:52:05 +0200

    add iicones

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import ProductList from './screens/ProductList';
 import Cart from './screens/Cart';
 import Profil from './screens/Profil';
 import React, {useEffect, useState} from 'react';
+
 import {
   Pressable,
   SafeAreaView,
@@ -12,9 +13,9 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View,
 } from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -26,9 +27,36 @@ const App = () => {
       <NavigationContainer>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={ProductList} />
-          <Tab.Screen name="Cart" component={Cart} />
-          <Tab.Screen name="Profil" component={Profil} />
+          <Tab.Screen
+            name="Home"
+            component={ProductList}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({color}) => (
+                <Icon name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={Cart}
+            options={{
+              tabBarLabel: 'Cart',
+              tabBarIcon: ({color}) => (
+                <Icon name="cart" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profil"
+            component={Profil}
+            options={{
+              tabBarLabel: 'Profil',
+              tabBarIcon: ({color}) => (
+                <Icon name="account" color={color} size={26} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
